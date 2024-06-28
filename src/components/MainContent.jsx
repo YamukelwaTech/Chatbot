924ea02ba414587387c87a58dcddd170477fd802
YamuTech <157011521+YamukelwaTech@users.noreply.github.com>
2024-06-28 12:22:35 +0200

    Chore :styling

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -44,20 +44,18 @@ const MainContent = () => {
   return (
     <main className="flex flex-col items-center justify-center h-screen bg-red-500 p-6">
       <div className="flex-1 flex flex-col justify-between items-center w-full">
+        <div className="w-full text-left mb-6 bg-orange-700 pl-9">
+          <h3 className="text-lg font-semibold">Get answers in seconds</h3>
+          <p className="text-gray-500">Create and complete tasks using boards</p>
+        </div>
         <div className="overflow-auto p-4 h-[70vh] w-[90vw] lg:h-[70vh] lg:w-[60vw] bg-white rounded-lg shadow-md">
           {messages.length === 0 ? (
-            <div className="p-6 text-center">
-              <h3 className="text-lg font-semibold">Get answers in seconds</h3>
-              <p className="text-gray-500 mt-4">
-                Create and complete tasks using boards
+            <div className="h-1/2 bg-yellow-500 p-6 rounded-lg shadow-md mb-6 flex flex-col justify-center items-center">
+              <h4 className="font-medium">Search History</h4>
+              <p className="text-gray-500">No Questions added</p>
+              <p className="text-gray-500">
+                Type your questions below to get fast answers
               </p>
-              <div className="bg-gray-200 p-4 rounded-lg mt-6">
-                <h4 className="font-medium">Search History</h4>
-                <p className="text-gray-500">No Questions added</p>
-                <p className="text-gray-500">
-                  Type your questions below to get fast answers
-                </p>
-              </div>
             </div>
           ) : (
             <div className="mt-4 flex flex-col space-y-4 overflow-y-auto max-h-full">
